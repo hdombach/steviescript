84b344c5417ef70daa63dd4d0aa480f9b9b4b49b
Hezekiah Dombach <hezekiahdombach@gmail.com>
2021-10-22 15:00:14 -0400

    Added more nodes

--- Node.java
+++ Node.java
@@ -6,9 +6,9 @@ abstract public class Node {
 	private static int index = 0;
 	private static ArrayList<Token> tokens;
 	private static Block block;
-	protected static Integer printIndent = 0;
+	protected static Integer indent = 0;
 	private static String printIndentChar = "| ";
-	protected static String printIndentString() {
+	protected static String indentStr() {
 		var result = "";
 		for (Integer c = 0; printIndent > c; c++) {
 			result += printIndentChar;
@@ -84,16 +84,16 @@ class Block extends Node {
 	public ArrayList<Statement> statements = new ArrayList<Statement>();
 	public String toString() {
 		String result = "";
-		result += Node.printIndentString() + "Block:\n";
-		Node.printIndent++;
-		result += Node.printIndentString() + "statements: [\n";
-		Node.printIndent++;
+		result += Node.indentStr() + "Block:\n";
+		Node.indent++;
+		result += Node.indentStr() + "statements: [\n";
+		Node.indent++;
 		for (Integer c = 0; statements.size() > c; c++) {
 			result += statements.get(c).toString();
 		}
-		Node.printIndent--;
-		result += Node.printIndentString() + "]\n";
-		Node.printIndent--;
+		Node.indent--;
+		result += Node.indentStr() + "]\n";
+		Node.indent--;
 		return result;
 		
 	}
@@ -106,19 +106,53 @@ class Set extends Statement {
 	public Expression expression;
 	public String toString() {
 		String result = "";
-		result += Node.printIndentString() + "Statement " + name + ": \n";
-		Node.printIndent++;
-		result += Node.printIndentString() + "expression: \n";
-		Node.printIndent++;
+		result += Node.indentStr() + "Statement " + name + ": \n";
+		Node.indent++;
+		result += Node.indentStr() + "expression: \n";
+		Node.indent++;
 		result += expression.toString();
-		Node.printIndent--;
-		Node.printIndent--;
+		Node.indent--;
+		Node.indent--;
 		return result;
 	}
 }
 
 class Expression extends Node {
+	public Node content;
 	public String toString() {
-		return Node.printIndentString() + "Expression\n";
+		String result = "";
+		result += Node.intentStr() + "Expression\n";
+		Node.indent++;
+		result += Node.indentStr() + "content: \n";
+		Node.indent++;
+		result += content.toString();
+		Node.indnet -= 2;
+		return result;
+	}
+}
+
+class VariableName extends Node {
+	public String name;
+	public String toString() {
+		return Node.indentStr() + "Variable: " + name + "\n";
+	}
+}
+
+class Operation extends Node {
+	public String operator;
+	public Expression left;
+	public Expression right;
+	public String toString() {
+		String result = Node.indentStr() + "Operation: " + operator + "\n";
+		Node.indent++;
+		result += Node.indentStr() + "Left: \n";
+		Node.indent++;
+		result += left.toString();
+		Node.indent--;
+		result += Node.indentStr() + "Right: \n";
+		Node.indent++;
+		result += right.toString();
+		Node.indent -= 2;
+		return result;
 	}
 }
