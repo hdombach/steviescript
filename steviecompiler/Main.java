a8769601661eb99414e69d2a0d8cfb53edbd21f8
Benjome <67120619+Benjome@users.noreply.github.com>
2021-10-26 22:05:58 -0400

    Main now stores all code it reads

--- steviecompiler/Main.java
+++ steviecompiler/Main.java
@@ -2,26 +2,34 @@ package steviecompiler;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 import steviecompiler.node.Node;
+import steviecompiler.error.ErrorHandler;
 
 
 public class Main{   
     public static String filePath;
+    public static HashMap<String, ArrayList<String>> codeText = new HashMap<String, ArrayList<String>>();
     public static void main(String[] args){
       filePath = args[0]; 
       readFile(filePath);
       //System.out.println(Token.getTokenList());
       Node.parse(Token.getTokenList());
       System.out.println(Node.getCode());
+      ErrorHandler.throwErrors();
     }
 
     public static void readFile(String path) {
       try {
         Scanner reader = new Scanner(new File(path));
+        codeText.put(path, new ArrayList<String>());
         int line = 1;
         while(reader.hasNextLine()) {
-          Token.tokenize(reader.nextLine(), line);
+          String thisLine = reader.nextLine();
+          codeText.get(path).add(thisLine);
+          Token.tokenize(thisLine, line);
           line++;
         }
         reader.close();
@@ -30,6 +38,10 @@ public class Main{
         e.printStackTrace();
         System.exit(1);
       }
+    }
+
+    public static String getLine(int line) {
+      return codeText.get(filePath).get(line - 1);
     }
 }
 
