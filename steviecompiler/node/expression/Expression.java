57463b48cf977b7f6e66a254beacad149390bef8
Hezekiah Dombach <hezekiahdombach@gmail.com>
2021-10-29 17:42:57 -0400

    can nest expressions in ()

--- steviecompiler/node/expression/Expression.java
+++ steviecompiler/node/expression/Expression.java
@@ -54,6 +54,10 @@ public abstract class Expression extends Node {
 		if (e.isValid) { return loadMethods(e); }
 
 		Node.index = beginIndex;
+		e = new VariableExpression();
+		if (e.isValid) { return loadMethods(e); }
+
+		Node.index = beginIndex;
 		if (Node.currentToken().getType() == TokenType.OPENPARAN) {
 			Node.index++;
 			e = Expression.expect();
