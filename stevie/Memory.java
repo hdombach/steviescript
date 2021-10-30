76a910b34e1979f9ac11f2e194e2c03f0d3a7ab6
Hezekiah Dombach <hezekiahdombach@gmail.com>
2021-10-29 22:22:15 -0400

    Simplified memorry

--- stevie/Memory.java
+++ stevie/Memory.java
@@ -4,25 +4,17 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Memory {
-	private static ArrayList<Byte> cont;
+	private static ArrayList<ArrayList<Byte>> cont;
 	
 	public static void init() {
-		cont = new ArrayList<Byte>();
+		cont = new ArrayList<ArrayList<Byte>>();
 	}
 
 	public static ArrayList<Byte> get(int address, Integer length) {
-		ArrayList<Byte> result = new ArrayList<Byte>();
-		int start;
-		if (0 > address) {
-			start = cont.size() - address;
+		if (address < 0) {
+			return cont.get(cont.size() - address);
 		} else {
-			start = address;
+			return cont.get(address);
 		}
-		int c = 0;
-		while (length > c) {
-			result.add(cont.get(start + c));
-			c += 1;
-		}
-		return result;
 	}
 }
