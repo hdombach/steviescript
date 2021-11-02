8d169d64dbab6dc0c59918e04f03a05a7d38f7f3
Hezekiah Dombach <hezekiahdombach@gmail.com>
2021-11-02 12:36:28 -0400

    Added comments to help explain what is happening in Memory

--- stevie/Memory.java
+++ stevie/Memory.java
@@ -6,6 +6,30 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
+/*
+
+Memory
+
+[heap] //heap includes code and memory provided by aloc
+[heap]
+[heap]
+[heap]
+[...] //empty space for if heap grows later on
+[...]
+[...]
+[stack] //items are pushed onto end of stack
+[stack]
+[stack]
+[...] //empty space
+[...]
+[...]
+
+if add >= 0 then count from start of heap.
+if add < 0 then count backwards from end of stack.
+
+*/
+
 public class Memory {
 	private static ArrayList<MetaData> metaData;
 	private static ByteBuffer bytes;
@@ -22,19 +46,23 @@ public class Memory {
 		metaData.add(new MetaData(0, startingHeapSize));
 	}
 
+	//adds to end of stack
 	public static void push(int length) {
 		bytes.put(stackSize, new byte[length]);
 		stackSize += length;
 	}
+	//adds to end of stack
 	public static void push(byte[] data) {
 		bytes.put(stackSize, data);
 		stackSize += data.length;
 	}
 
+	//removes form end of stack.
 	public static void pop(int length) {
 		stackSize -= length;
 	}
 
+	//Finds memory in heap.
 	public static int alloc(int length) {
 		int c = 0;
 		while (metaData.size() > c) {
@@ -57,6 +85,7 @@ public class Memory {
 		//TODO: allocated more memory
 	}
 
+	//frees memory in heap and simplifies the metadata
 	public static void free(int address) {
 		int c = 0;
 		MetaData last = null;
@@ -88,11 +117,13 @@ public class Memory {
 		}
 	}
 
+	
 	public static void set(int address, byte[] value) {
-		bytes.put(address, value);
+		bytes.put(normAdd(address), value);
 	}
 
 	//Normalizes the adress
+	//takes into account negative numbers
 	private static int normAdd(int address) {
 		if (address < 0) {
 			return heapSize + stackSize + address;
